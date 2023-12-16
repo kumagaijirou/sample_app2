0df740372ff0503a2639a81b78fb27b3961e2c3e
kumagaijirou <55349713+kumagaijirou@users.noreply.github.com>
2023-12-16 14:28:28 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert'                            # 2.1. class="alert"が存在するか
     assert_select 'div.alert-danger'              # 2.2. class="alert-danger"が存在するか
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { name: "Example User", 
+                          email: "user@example.com", 
+                          password: "password",
+                          password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
